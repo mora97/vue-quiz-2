2b6f3838eaebcc73d5ab17420b19d34408a7c43c
Amirhossein Payon <a.payon@karboom.io>
2020-11-19 14:05:02 +0330

    initial

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <h1>Hello Dear Friend! \ (•◡•) /</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+  .app {
+    text-align: center;
+  }
+</style>
